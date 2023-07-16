6332414619e23669d0648104158b8023a922d911
mateusbrbza <mateus.c.barboza@gmail.com>
2023-07-16 18:20:23 -0300

    92/101: finalizando edicao de contato

--- src/pages/EditContact/index.js
+++ src/pages/EditContact/index.js
@@ -10,6 +10,8 @@ import toast from '../../utils/toast';
 
 export default function EditContact() {
   const [isLoading, setIsLoading] = useState(true);
+  const [contactName, setContactName] = useState('');
+
   const contactFormRef = useRef(null);
 
   const { id } = useParams();
@@ -24,6 +26,7 @@ export default function EditContact() {
 
         contactFormRef.current.setFieldsValues(contact);
         setIsLoading(false);
+        setContactName(contact.name);
       } catch {
         history.push('/');
         toast({
@@ -36,14 +39,37 @@ export default function EditContact() {
     loadContact();
   }, [history, id]);
 
-  function handleSubmit() {}
+  async function handleSubmit(formData) {
+    try {
+      const contact = {
+        name: formData.name,
+        email: formData.email,
+        phone: formData.phone,
+        category_id: formData.categoryId,
+      };
+
+      const contactData = await ContactsService.updateContact(id, contact);
+
+      setContactName(contactData.name);
+      toast({
+        type: 'success',
+        text: 'Contato editado com sucesso!',
+        duration: 4000,
+      });
+    } catch {
+      toast({
+        type: 'danger',
+        text: 'Ocorreu um erro ao editar o contato!',
+      });
+    }
+  }
 
   return (
     <>
       <Loader isLoading={isLoading} />
 
       <PageHeader
-        title="Editar contato"
+        title={isLoading ? 'Carregando...' : `Editar ${contactName}`}
       />
 
       <ContactForm
